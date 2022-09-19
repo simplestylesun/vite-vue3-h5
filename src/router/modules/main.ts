c82c87a050e82237b17e4e9f3f5b093c4dc8202a
Wu Kunling <13022195532@163.com>
2022-09-19 09:56:55 +0800

    add

--- src/router/modules/main.ts
+++ src/router/modules/main.ts
@@ -1,3 +1,9 @@
+/*
+ * @Date: 2022-09-15 11:01:52
+ * @LastEditors: wkl
+ * @LastEditTime: 2022-09-15 13:36:07
+ * @FilePath: /vite-vue3-h5/src/router/modules/main.ts
+ */
 import { RouteRecordRaw } from 'vue-router';
 
 export const main: Array<RouteRecordRaw> = [
@@ -16,6 +22,15 @@ export const main: Array<RouteRecordRaw> = [
     component: () => import('@/views/category/index.vue'),
     meta: {
       title: '分类',
+      icon: 'apps-o',
+    },
+  },
+  {
+    path: '/demo',
+    name: 'demo',
+    component: () => import('@/views/demo/index.vue'),
+    meta: {
+      title: 'demo',
       icon: 'apps-o',
     },
   },
---
+++ src/views/demo/index.vue
@@ -0,0 +1,14 @@
+<!--
+ * @Date: 2022-09-15 13:36:42
+ * @LastEditors: wkl
+ * @LastEditTime: 2022-09-15 14:00:55
+ * @FilePath: /vite-vue3-h5/src/views/demo/index.vue
+-->
+<template>
+  <div class="flex-center"> <div>aaaa</div><div>bbb</div><div>aaccccaa</div> </div>
+</template>
+<style lang="scss" scoped>
+  .flex-center {
+    font-size: rem2px(0.5);
+  }
+</style>
